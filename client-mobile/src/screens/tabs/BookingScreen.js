3cbf9f06ddebb9b8104ac6dd2ce94906b208d91e
Tomthedeveloper <tomthedeveloper11@gmail.com>
2022-04-18 01:38:49 +0700

    clean booking screen

--- client-mobile/src/screens/tabs/BookingScreen.js
+++ client-mobile/src/screens/tabs/BookingScreen.js
@@ -27,108 +27,11 @@ function BookingScreen({ navigation }) {
 //         </TouchableOpacity>
 //       </View>
 //   )
-  const [facialRecognitionAvailable, setFacialRecognitionAvailable] =
-    React.useState(false)
-  const [fingerprintAvailable, setFingerprintAvailable] = React.useState(false)
-  const [irisAvailable, setIrisAvailable] = React.useState(false)
-  const [loading, setLoading] = React.useState(false)
-  const [result, setResult] = React.useState()
-
-  const checkSupportedAuthentication = async () => {
-    const types = await LocalAuthentication.supportedAuthenticationTypesAsync()
-    if (types && types.length) {
-      setFacialRecognitionAvailable(
-        types.includes(
-          LocalAuthentication.AuthenticationType.FACIAL_RECOGNITION
-        )
-      )
-      setFingerprintAvailable(
-        types.includes(LocalAuthentication.AuthenticationType.FINGERPRINT)
-      )
-      setIrisAvailable(
-        types.includes(LocalAuthentication.AuthenticationType.IRIS)
-      )
-    }
-  }
-  const authenticate = async () => {
-    if (loading) {
-      return
-    }
-
-    setLoading(true)
-
-    try {
-      const results = await LocalAuthentication.authenticateAsync()
-
-      if (results.success) {
-        setResult('success')
-      } else if (results.error === 'unknown') {
-        setResult('disabled')
-      } else if (
-        results.error === 'user_cancel' ||
-        results.error === 'system_cancel' ||
-        results.error === 'app_cancel'
-      ) {
-        setResult('cancelled')
-      }
-    } catch (error) {
-      setResult('error')
-    }
-
-    setLoading(false)
-  }
-
-  React.useEffect(() => {
-    checkSupportedAuthentication()
-  }, [])
-
-  let resultMessage
-  switch (result) {
-    case 'cancelled':
-      resultMessage = 'Authentication process has been cancelled'
-      break
-    case 'disabled':
-      resultMessage = 'Biometric authentication has been disabled'
-      break
-    case 'error':
-      resultMessage = 'There was an error in authentication'
-      break
-    case 'success':
-      resultMessage = 'Successfully authenticated'
-      break
-    default:
-      resultMessage = ''
-      break
-  }
-
-  let description
-  if (facialRecognitionAvailable && fingerprintAvailable && irisAvailable) {
-    description = 'Authenticate with Face ID, touch ID or iris ID'
-  } else if (facialRecognitionAvailable && fingerprintAvailable) {
-    description = 'Authenticate with Face ID or touch ID'
-  } else if (facialRecognitionAvailable && irisAvailable) {
-    description = 'Authenticate with Face ID or iris ID'
-  } else if (fingerprintAvailable && irisAvailable) {
-    description = 'Authenticate with touch ID or iris ID'
-  } else if (facialRecognitionAvailable) {
-    description = 'Authenticate with Face ID'
-  } else if (fingerprintAvailable) {
-    description = 'Authenticate with touch ID '
-  } else if (irisAvailable) {
-    description = 'Authenticate with iris ID'
-  } else {
-    description = 'No biometric authentication methods available'
-  }
+  
 
   return (
     <View style={{marginTop: 250}}>
-      <Text>{description}</Text>
-      {facialRecognitionAvailable || fingerprintAvailable || irisAvailable ? (
-        <Button onPress={authenticate} title={'Authenticate'}>
-          Authenticate
-        </Button>
-      ) : null}
-      {resultMessage ? <Text>{resultMessage}</Text> : null}
+      <Text>Booking</Text>
     </View>
   )
 }
