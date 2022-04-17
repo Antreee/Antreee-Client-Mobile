29715e66c221f5d95e4c85337728b688d8d1684c
Tomthedeveloper <tomthedeveloper11@gmail.com>
2022-04-18 01:04:56 +0700

    Cart is working until xendit payment

--- client-mobile/src/screens/tabs/QrScanScreen.js
+++ client-mobile/src/screens/tabs/QrScanScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { RestaurantContext } from "../../components/Context";
 
